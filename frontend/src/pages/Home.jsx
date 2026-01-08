764d96e4141fda0627fc93164fd32cb4d8194281
MK596 <amirinenimanikanta@gmail.com>
2026-01-08 12:20:39 +0530

    Fix: resolve frontend crash by adding missing FaCheckCircle import in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { FaPlus, FaTicketAlt, FaUserShield, FaArrowRight, FaSearch, FaBolt, FaQuestionCircle, FaShieldAlt } from 'react-icons/fa';
+import { FaPlus, FaTicketAlt, FaUserShield, FaArrowRight, FaSearch, FaBolt, FaQuestionCircle, FaShieldAlt, FaCheckCircle } from 'react-icons/fa';
 import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import AuthContext from '../context/AuthContext';
